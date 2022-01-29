19db18c1b9b0c153cd4d443dc226cafc9ed3d707
3uky <hynek.bucek@gmail.com>
2022-01-29 18:25:55 +0100

    decoding tables moved to separate file

---
+++ include/decoding_tables.h
@@ -0,0 +1,47 @@
+#ifndef DECODING_TABLES_H_INCLUDED
+#define DECODING_TABLES_H_INCLUDED
+
+#include "instruction.h"
+#include "argument.h"
+
+namespace instruction {
+    std::map<std::pair<VM_BYTE, int>, Instruction::type> table = {
+        { {0b000, 3}, Instruction::type::mov },
+        { {0b001, 3}, Instruction::type::loadConstant },
+        { {0b010001, 6}, Instruction::type::add },
+        { {0b010010, 6}, Instruction::type::sub },
+        { {0b010011, 6}, Instruction::type::div },
+        { {0b010100, 6}, Instruction::type::mod },
+        { {0b010101, 6}, Instruction::type::mul },
+        { {0b01100, 5}, Instruction::type::compare },
+        { {0b01101, 5}, Instruction::type::jump },
+        { {0b01110, 5}, Instruction::type::jumpEqual },
+        { {0b10000, 5}, Instruction::type::read },
+        { {0b10001, 5}, Instruction::type::write },
+        { {0b10010, 5}, Instruction::type::consoleRead },
+        { {0b10011, 5}, Instruction::type::consoleWrite },
+        { {0b10100, 5}, Instruction::type::createThread },
+        { {0b10101, 5}, Instruction::type::joinThread },
+        { {0b10110, 5}, Instruction::type::hlt },
+        { {0b10111, 5}, Instruction::type::sleep },
+        { {0b1100, 4}, Instruction::type::call },
+        { {0b1101, 4}, Instruction::type::ret },
+        { {0b1110, 4}, Instruction::type::lock },
+        { {0b1111, 4}, Instruction::type::unlock}
+    };
+}
+
+namespace argument {
+    std::map<VM_BYTE, Argument::memSize> sizeTable = {
+        { 0b00, Argument::memSize::byte },
+        { 0b01, Argument::memSize::word },
+        { 0b10, Argument::memSize::dword },
+        { 0b11, Argument::memSize::qword }
+    };
+    std::map<VM_BYTE, Argument::type> typeTable = {
+        { 1, Argument::type::mem },
+        { 0, Argument::type::reg }
+    };
+}
+
+#endif // DECODING_TABLES_H_INCLUDED
--- src/decoder.cpp
+++ src/decoder.cpp
@@ -1,49 +1,9 @@
 #include <map>
 
 #include "../include/decoder.h"
-// #include "../include/decoding_tables.h" // tbd move tables here
+#include "../include/decoding_tables.h"
 
 using namespace std;
-
-namespace instruction {
-    std::map<std::pair<VM_BYTE, int>, Instruction::type> table = {
-        { {0b000, 3}, Instruction::type::mov },
-        { {0b001, 3}, Instruction::type::loadConstant },
-        { {0b010001, 6}, Instruction::type::add },
-        { {0b010010, 6}, Instruction::type::sub },
-        { {0b010011, 6}, Instruction::type::div },
-        { {0b010100, 6}, Instruction::type::mod },
-        { {0b010101, 6}, Instruction::type::mul },
-        { {0b01100, 5}, Instruction::type::compare },
-        { {0b01101, 5}, Instruction::type::jump },
-        { {0b01110, 5}, Instruction::type::jumpEqual },
-        { {0b10000, 5}, Instruction::type::read },
-        { {0b10001, 5}, Instruction::type::write },
-        { {0b10010, 5}, Instruction::type::consoleRead },
-        { {0b10011, 5}, Instruction::type::consoleWrite },
-        { {0b10100, 5}, Instruction::type::createThread },
-        { {0b10101, 5}, Instruction::type::joinThread },
-        { {0b10110, 5}, Instruction::type::hlt },
-        { {0b10111, 5}, Instruction::type::sleep },
-        { {0b1100, 4}, Instruction::type::call },
-        { {0b1101, 4}, Instruction::type::ret },
-        { {0b1110, 4}, Instruction::type::lock },
-        { {0b1111, 4}, Instruction::type::unlock}
-    };
-}
-
-namespace argument {
-    std::map<VM_BYTE, Argument::memSize> sizeTable = {
-        { 0b00, Argument::memSize::byte },
-        { 0b01, Argument::memSize::word },
-        { 0b10, Argument::memSize::dword },
-        { 0b11, Argument::memSize::qword }
-    };
-    std::map<VM_BYTE, Argument::type> typeTable = {
-        { 1, Argument::type::mem },
-        { 0, Argument::type::reg }
-    };
-}
 
 Decoder::Decoder(Memory& mem, Registers& reg) : memory(mem), ip(reg.ip)
 {
