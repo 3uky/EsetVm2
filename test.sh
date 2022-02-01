5fecc8dc81b15792d073e920f248f767a5302518
3uky <hynek.bucek@gmail.com>
2022-02-01 20:17:32 +0100

    regression tests

---
+++ test.sh
@@ -0,0 +1,19 @@
+!#/bin/bash
+BINARY_DIR=./task/samples/precompiled
+make clean
+make 
+
+echo math.evm
+./esetvm2 $BINARY_DIR/math.evm
+
+echo memory.evm
+./esetvm2 $BINARY_DIR/memory.evm
+
+echo xor.evm
+echo 11 29 | ./esetvm2 $BINARY_DIR/xor.evm
+
+echo xor-with-stack-frame.evm
+echo 11 29 | ./esetvm2 $BINARY_DIR/xor-with-stack-frame.evm
+
+echo crc.evm
+./esetvm2 $BINARY_DIR/crc.evm
