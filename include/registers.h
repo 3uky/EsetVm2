104f0806a9a7bbcd7104f9c5b7aa0d73a9e818dd
3uky <hynek.bucek@gmail.com>
2022-02-03 12:25:30 +0100

    registers class refactoring

--- include/registers.h
+++ include/registers.h
@@ -11,30 +11,23 @@ const int REGS_COUNT=16;
 class Registers
 {
 public:
-    /* Here will be stored all registers */
+    // all registers
     std::array<int64_t, REGS_COUNT> reg;
-    /* Instruction Pointer / Bit Pointer */
+    // Instruction Pointer / Bit Pointer
     VM_DWORD ip;
-    /* Stack Pointer */
+    // Stack Pointer
     std::stack<VM_DWORD> sp;
+
+    uint64_t instcount;
+    size_t tId;
 
     int64_t& operator[](int index) {
         return reg[index];
     }
 
-    uint64_t instcount;
-
-    void print() const {
-        std::cout << "ip = " << std::dec << ip << std::endl;
-        if(!sp.empty())
-            std::cout << "sp = " << std::dec << sp.top() << std::endl;
-        for (int i=0; i<REGS_COUNT; i++)
-            std::cout << "r[" << std::dec << i << "] = 0x" << std::hex << reg[i] << std::endl;
-        std::cout << std::endl;
-    }
-    void printInstCounter() const {
-        std::cout << std::dec << instcount << ": ";
-    }
+    void print() const;
+    void printInstCounter() const;
+    void emptyStack();
 };
 
 #endif // REGISTERS_H
---
+++ src/registers.cpp
@@ -0,0 +1,22 @@
+#include "../include/registers.h"
+
+using namespace std;
+
+void Registers::print() const {
+    cout << "ip = " << dec << ip << endl;
+    if(!sp.empty())
+        cout << "sp = " << dec << sp.top() << endl;
+    for (int i=0; i<REGS_COUNT; i++)
+        cout << "r[" << dec << i << "] = 0x" << hex << reg[i] << endl;
+    cout << endl;
+}
+
+void Registers::printInstCounter() const {
+    cout << dec << instcount << ": ";
+}
+
+void Registers::emptyStack()
+{
+    while (!sp.empty())
+        sp.pop();
+}
