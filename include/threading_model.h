ceebd03e835bea4d63a53a744e465e78c46bf965
3uky <hynek.bucek@gmail.com>
2022-02-03 15:24:30 +0100

    thread model - dynamic mutex initialization, lock, unlock

--- include/threading_model.h
+++ include/threading_model.h
@@ -1,13 +1,30 @@
 #ifndef THREADING_MODEL_H_INCLUDED
 #define THREADING_MODEL_H_INCLUDED
 
+#include <vector>
+#include <map>
 #include <thread>
+#include <mutex>
+
+#include "global.h"
 
 class ThreadingModel
 {
 public:
     std::vector<std::thread> threads;
-    // tbd locks/mutexes
+    std::map<VM_QWORD, std::mutex> mtxs;
+    //std::mutex mtx;
+
+    void lock(VM_QWORD index) {
+        //mtx.lock();
+        mtxs.emplace(std::piecewise_construct, std::make_tuple(index), std::make_tuple());
+        mtxs[index].lock();
+    };
+
+    void unlock(VM_QWORD index) {
+        //mtx.unlock();
+        mtxs[index].unlock(); // tbd: check if exists
+    };
 };
 
 #endif // THREADING_MODEL_H_INCLUDED
