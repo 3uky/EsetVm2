1aa536069468b3cefebb1fbd3b71a792252b2f40
3uky <hynek.bucek@gmail.com>
2022-02-05 20:36:14 +0100

    regression tests refactoring

--- test.sh
+++ test.sh
@@ -1,22 +1,34 @@
 !#/bin/bash
+
 BINARY_DIR=./task/samples/precompiled
 RESULT_DIR=./analysis/results
 TMP_DIR=./res
 
-function check_result() {
-	if diff -q --strip-trailing-cr $TMP_DIR/$1 $RESULT_DIR/$1 &>/dev/null; then
-		echo -e "\e[1;32m TEST OK\033[0m"
-	else
-		echo -e "\e[1;31m TEST FAILED\033[0m"
+function test() {
+	if [ $# -eq 1 ]; then
+		echo $1.evm
+		./esetvm2 $BINARY_DIR/$1.evm | tee $TMP_DIR/$1.res
+		check_result $1.res
+	elif [ $# -eq 2 ]; then
+		echo $1.evm \(input: $2\)
+		echo $2 | ./esetvm2 $BINARY_DIR/$1.evm | tee $TMP_DIR/$1.res
+		check_result $1.res
 	fi
 }
 
+function check_result() {
+	RESULT=""
+	if [ $# -eq 1 ]; then
+		RESULT=$(diff -q --strip-trailing-cr $TMP_DIR/$1 $RESULT_DIR/$1)
+	elif [ $# -eq 2 ]; then
+		RESULT=$(diff -q --strip-trailing-cr $1 $2)
+	fi
 
-function check_result_a_b() {
-	if diff -q --strip-trailing-cr $1 $2 &>/dev/null; then
+	if [ -z "$RESULT" ]; then
 		echo -e "\e[1;32m TEST OK\033[0m"
 	else
 		echo -e "\e[1;31m TEST FAILED\033[0m"
+		echo $RESULT
 	fi
 }
 
@@ -25,43 +37,19 @@ mkdir $TMP_DIR
 make clean
 make 
 
-echo math.evm
-./esetvm2 $BINARY_DIR/math.evm | tee $TMP_DIR/math.res
-check_result math.res
-
-echo memory.evm
-./esetvm2 $BINARY_DIR/memory.evm | tee $TMP_DIR/memory.res
-check_result memory.res
-
-echo fibonacci_loop.evm \(input: 10\)
-echo 10 | ./esetvm2 $BINARY_DIR/fibonacci_loop.evm | tee $TMP_DIR/fibonacci_loop.res
-check_result fibonacci_loop.res
-
-echo xor.evm \(input: 11 29\)
-echo 11 29 | ./esetvm2 $BINARY_DIR/xor.evm | tee $TMP_DIR/xor.res
-check_result xor.res
-
-echo xor-with-stack-frame.evm
-echo 11 29 | ./esetvm2 $BINARY_DIR/xor-with-stack-frame.evm | tee $TMP_DIR/xor-with-stack-frame.res
-check_result xor-with-stack-frame.res
-
-echo crc.evm
-./esetvm2 $BINARY_DIR/crc.evm | tee $TMP_DIR/crc.res
-check_result crc.res
-
-echo threadingBase.evm
-./esetvm2 $BINARY_DIR/threadingBase.evm | tee $TMP_DIR/threadingBase.res
-check_result threadingBase.res
-
-echo lock.evm
-./esetvm2 $BINARY_DIR/lock.evm | tee $TMP_DIR/lock.res
-check_result lock.res
-
+test math
+test memory
+test fibonacci_loop 10
+test xor "11 29"
+test xor-with-stack-frame "11 29"
+test crc
+test threadingBase
+test lock
 rm -dr $TMP_DIR
 
 echo multithreaded_file_write.evm
 ./esetvm2 $BINARY_DIR/multithreaded_file_write.evm
-check_result_a_b task/samples/multithreaded_file_write.bin analysis/results/multithreaded_file_write.bin
+check_result task/samples/multithreaded_file_write.bin analysis/results/multithreaded_file_write.bin
 
 echo pseudorandom.evm \(check manually\)
 sleep 0.0$((($RANDOM % 100))); echo 1 | ./esetvm2 $BINARY_DIR/pseudorandom.evm
