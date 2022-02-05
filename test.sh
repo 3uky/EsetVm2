3e521da24db6c8825d831307754257b619c97a1d
3uky <hynek.bucek@gmail.com>
2022-02-05 20:56:54 +0100

    regression test - pseudo-random generator test update

--- test.sh
+++ test.sh
@@ -18,20 +18,24 @@ function test() {
 
 function check_result() {
 	RESULT=""
+
 	if [ $# -eq 1 ]; then
 		RESULT=$(diff -q --strip-trailing-cr $TMP_DIR/$1 $RESULT_DIR/$1)
 	elif [ $# -eq 2 ]; then
 		RESULT=$(diff -q --strip-trailing-cr $1 $2)
 	fi
 
-	if [ -z "$RESULT" ]; then
+	print_result $RESULT
+}
+
+function print_result() {
+	if [ -z "$1" ]; then
 		echo -e "\e[1;32m TEST OK\033[0m"
 	else
 		echo -e "\e[1;31m TEST FAILED\033[0m"
-		echo $RESULT
+		echo $1
 	fi
 }
-
 mkdir $TMP_DIR
 
 make clean
@@ -51,8 +55,13 @@ echo multithreaded_file_write.evm
 ./esetvm2 $BINARY_DIR/multithreaded_file_write.evm
 check_result task/samples/multithreaded_file_write.bin analysis/results/multithreaded_file_write.bin
 
-echo pseudorandom.evm \(check manually\)
-sleep 0.0$((($RANDOM % 100))); echo 1 | ./esetvm2 $BINARY_DIR/pseudorandom.evm
+echo pseudorandom.evm \(run twice with different delay\)
+RES1=$(echo 1; sleep 0.011 | ./esetvm2 $BINARY_DIR/pseudorandom.evm)
+RES2=$(echo 2; sleep 0.012 | ./esetvm2 $BINARY_DIR/pseudorandom.evm)
+echo $RES1
+echo $RES2
+RES=""; [ "$RES1" == "$RES2" ] && RES="failed"
+print_result $RES
 
 echo philosophers.evm \(specify number of philosophers e.g. 5\)
 ./esetvm2 $BINARY_DIR/philosophers.evm
